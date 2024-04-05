a726c064d090f1eb3cc06d75eeca4536efaf1554
LionShareZoki <pujic.zoran@gmail.com>
2024-04-05 18:58:01 +0200

    add wishlist item

---
+++ backend/src/wishlist-items/wishlist-items.controller.ts
@@ -0,0 +1,73 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { WishlistItemsService } from './wishlist-items.service';
+import { CreateWishlistItemDto } from './dto/create-wishlist-item.dto';
+import { UpdateWishlistItemDto } from './dto/update-wishlist-item.dto';
+import { ApiTags, ApiBody, ApiOperation } from '@nestjs/swagger';
+import { UserAuthGuard } from 'src/users/user-auth.guard';
+
+@ApiTags('wishlist-items')
+@Controller('wishlist-items')
+export class WishlistItemsController {
+  constructor(private readonly wishlistItemsService: WishlistItemsService) {}
+  @UseGuards(UserActivation)
+  @Post()
+  @ApiOperation({ summary: 'Create a new wishlist item' })
+  @ApiBody({
+    description: 'Example of Wishlist Item POST request',
+    type: CreateWishlistItemDto,
+    examples: {
+      example1: {
+        value: {
+          id: 2,
+          wishlistId: 2,
+          productId: 1,
+        },
+        summary: 'Example of Wishlist Item POST request body',
+      },
+    },
+  })
+  create(@Body() createWishlistItemDto: CreateWishlistItemDto) {
+    return this.wishlistItemsService.create(createWishlistItemDto);
+  }
+
+  @UseGuards(UserAuthGuard)
+  @Get()
+  @ApiOperation({ summary: 'Get all wishlist items' })
+  findAll() {
+    return this.wishlistItemsService.findAll();
+  }
+
+  @UseGuards(UserAuthGuard)
+  @Get(':id')
+  @ApiOperation({ summary: 'Get specific wishlist item' })
+  findOne(@Param('id', ParseIntPipe) id: string) {
+    return this.wishlistItemsService.findOne(+id);
+  }
+
+  @UseGuards(UserAuthGuard)
+  @Patch(':id')
+  @ApiOperation({ summary: 'Update specific wishlist item' })
+  update(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() updateWishlistItemDto: UpdateWishlistItemDto,
+  ) {
+    return this.wishlistItemsService.update(+id, updateWishlistItemDto);
+  }
+
+  @UseGuards(UserAuthGuard)
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete specific wishlist item' })
+  remove(@Param('id', ParseIntPipe) id: string) {
+    return this.wishlistItemsService.remove(+id);
+  }
+}
