5348c53054b79e0f18c35849ebfa2c92d42eea7b
LionShareZoki <pujic.zoran@gmail.com>
2024-04-05 18:59:01 +0200

    update cart

---
+++ backend/src/carts/carts.controller.ts
@@ -0,0 +1,93 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { CartsService } from './carts.service';
+import { CreateCartDto } from './dto/create-cart.dto';
+import { UpdateCartDto } from './dto/update-cart.dto';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+} from '@nestjs/swagger';
+import { UserAuthGuard } from 'src/users/user-auth.guard';
+import { AdminAuthGuard } from 'src/users/admin-auth.guard';
+
+@ApiTags('Carts')
+@Controller('carts')
+export class CartsController {
+  constructor(private readonly cartsService: CartsService) {}
+  @UseGuards(UserAuthGuard)
+  @Post()
+  @ApiOperation({ summary: 'Create a new cart' })
+  @ApiBody({
+    description: 'Example of cart item POST request',
+    type: CreateCartDto,
+    examples: {
+      example1: {
+        value: {
+          userId: 3,
+        },
+        summary: 'Example of User POST request body',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The cart has been successfully created.',
+  })
+  create(@Body() createCartDto: CreateCartDto) {
+    return this.cartsService.create(createCartDto);
+  }
+  @UseGuards(AdminAuthGuard)
+  @Get()
+  @ApiOperation({ summary: 'Get all carts' })
+  @ApiResponse({ status: 200, description: 'Returns all carts.' })
+  findAll() {
+    return this.cartsService.findAll();
+  }
+  @UseGuards(UserAuthGuard)
+  @Get(':id')
+  @ApiOperation({ summary: 'Get a cart by ID' })
+  @ApiParam({ name: 'id', description: 'Cart ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns the cart with the specified ID.',
+  })
+  findOne(@Param('id', ParseIntPipe) id: string) {
+    return this.cartsService.findOne(+id);
+  }
+  @UseGuards(UserAuthGuard)
+  @Patch(':id')
+  @ApiOperation({ summary: 'Update a cart by ID' })
+  @ApiParam({ name: 'id', description: 'Cart ID' })
+  @ApiBody({ type: UpdateCartDto })
+  @ApiResponse({
+    status: 200,
+    description: 'The cart has been successfully updated.',
+  })
+  @UseGuards(UserAuthGuard)
+  update(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) {
+    return this.cartsService.update(+id, updateCartDto);
+  }
+  @UseGuards(AdminAuthGuard)
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete a cart by ID' })
+  @ApiParam({ name: 'id', description: 'Cart ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'The cart has been successfully deleted.',
+  })
+  remove(@Param('id', ParseIntPipe) id: string) {
+    return this.cartsService.remove(+id);
+  }
+}
